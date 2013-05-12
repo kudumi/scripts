15bb42ccb1e451f027c42ad81045f9a530a4fd74
Eric Crosson <eric.s.crosson@utexas.edu>
2013-05-12 12:09:59 -0500

    Added pacman.conf to the config folder
    This was necessary because pacman-color has been removed from the
    AUR. Now the recommended technique is to uncomment the line "Color"
    from /etc/pacman.conf, which is what I did. Also, now each machine
    will have multilib and archlinuxfr libraries available by
    default (though there is still a chicken-and-egg problem where Dropbox
    needs to be installed via AUR).

--- nix/install-symlinks.sh
+++ nix/install-symlinks.sh
@@ -22,35 +22,36 @@ mkdir $HOME/.config
 mkdir $HOME/.config/pianobar
 
 # Directories to clear (caveat emptor)
-
 $remove $HOME/.emacs.d
 $remove $HOME/.config/awesome
 
-$link "$config/emacs/.emacs" $HOME/.emacs
-$link "$config/emacs/.emacs.d" $HOME/.emacs.d
-$link "$config/bash/bashrc" $HOME/.bashrc
-$link "$config/awesome" $HOME/.config/awesome
-$link "$config/gitconfig" $HOME/.gitconfig
-$link "$config/pianobar" $HOME/.config/pianobar/config
-$link "$config/rtorrentrc" $HOME/.rtorrent.rc
-$link "$config/screen/screenrc" $HOME/.screenrc
-$link "$config/ssh/config" $HOME/.ssh/config
-$link "$config/xbindkeysrc" $HOME/.xbindkeysrc
-$link "$config/xresources" $HOME/.Xresources
-$link "$config/yaourtrc" $HOME/.yaourtrc
-$link "$config/octaverc" $HOME/.octaverc
-$link "$config/.gnus.el" $HOME/.gnus.el
-$link "$config/.inputrc" $HOME/.inputrc
+$link $config/emacs/.emacs $HOME/.emacs
+$link $config/emacs/.emacs.d $HOME/.emacs.d
+$link $config/bash/bashrc $HOME/.bashrc
+$link $config/awesome $HOME/.config/awesome
+$link $config/gitconfig $HOME/.gitconfig
+$link $config/pianobar $HOME/.config/pianobar/config
+$link $config/rtorrentrc $HOME/.rtorrent.rc
+$link $config/screen/screenrc $HOME/.screenrc
+$link $config/ssh/config $HOME/.ssh/config
+$link $config/xbindkeysrc $HOME/.xbindkeysrc
+$link $config/xresources $HOME/.Xresources
+$link $config/yaourtrc $HOME/.yaourtrc
+$link $config/octaverc $HOME/.octaverc
+$link $config/.gnus.el $HOME/.gnus.el
+$link $config/.inputrc $HOME/.inputrc
 
-# Machine-specific
-if [ -e "$config/machines/`hostname`" ]; then
+# Machine specific
+if [[ -e $config/machines/`hostname` ]]; then
     for file in `ls -A1 "$config/machines/\`hostname\`/"`; do
 	$link $config/machines/`hostname`/$file $HOME/$file
     done
 fi
 
-case `uname -a` in		# prepare Windows startup
+case `uname -a` in		# prepare Windows
     *Cygwin* )
 	$hardlink "$config/../scripts/windows/rc.compat.bat" \
 	    "/cygdrive/c/Users/`whoami`/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/" ;;
+    *ARCH* )			# prepare Arch Linux
+	$link $config/pacman.conf /etc/pacman.conf
 esac
