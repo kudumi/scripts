4b14a78fb0242d273ee703fab3cfb4ad998c5dc2
Eric Crosson <eric.s.crosson@utexas.edu>
2013-06-11 15:58:37 -0500

    nascent script

---
+++ nix/color-path-symlinks.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Color definitions
+Color_Off='\e[0m'       # Text Reset
+
+# Variables
+path="`pwd`/"
+sym_color="\e[`dircolors -p | grep "symbolic link" | cut -d' ' -f 2`m"
+unset output
+
+# Short representation of $HOME sweet $HOME
+path=${path/"/home/`whoami`"/"~"}
+
+while [[ ${path} ]]; do
+    output=${output}${path%%/*}
+
+    if [[ `find $HOME${output#'~'} -maxdepth 0 -type l` && ${output} != '~' ]]; then
+	output=${output%/*}/${sym_color}${output##*/}${Color_Off}
+    fi
+
+    output=${output}/		# add the trailing /
+
+    if [[ $path =~ .*/.* ]]; then
+	path=${path#*/}
+    else
+	unset path
+    fi
+done
+
+echo ${output%/}		# final value
