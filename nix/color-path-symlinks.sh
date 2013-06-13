c0593e209e9d5c1a872d3a9d42f8e69501b20d6e
Eric Crosson <eric.s.crosson@utexas.edu>
2013-06-13 17:55:04 -0500

    hello color-path-symlinks.sh

--- nix/color-path-symlinks.sh
+++ nix/color-path-symlinks.sh
@@ -6,19 +6,21 @@ Color_Off='\e[0m'       # Text Reset
 # Variables
 path="`pwd`/"
 sym_color="\e[`dircolors -p | grep "symbolic link" | cut -d' ' -f 2`m"
-unset output
+unset output path_sans_colors
 
 # Short representation of $HOME sweet $HOME
 #path=${path/?"~"/"/home/`whoami`"}
 
 while [[ ${path} ]]; do
     output=${output}${path%%/*}
+    path_sans_colors=${path_sans_colors}${path%%/*}
 
-    if [[ `find ${output#'~'} -maxdepth 0 -type l` ]]; then
+    if [[ `find ${path_sans_colors} -maxdepth 0 -type l` ]]; then
 	output=${output%/*}/${sym_color}${output##*/}${Color_Off}
     fi
 
     output=${output}/		# add the trailing /
+    path_sans_colors=${path_sans_colors}/
 
     if [[ $path =~ .*/.* ]]; then
 	path=${path#*/}
@@ -27,4 +29,5 @@ while [[ ${path} ]]; do
     fi
 done
 
-echo ${output%/}		# final value
+output=~${output#"$HOME"}       # short path
+echo -e ${output%/}		# final value
