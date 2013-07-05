266357e7fad17af8a27b84924195c2fce9fc9135
Eric Crosson <eric.s.crosson@utexas.edu>
2013-07-05 12:39:55 -0500

    No splash screen

--- nix/lock.sh
+++ nix/lock.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # This script locks the screen via xscreensaver daemon
+[[ -z `xscreensaver-command -time 2>/dev/null` ]] && xscreensaver -no-splash &
 xscreensaver-command --activate
