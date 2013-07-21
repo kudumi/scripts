256b860697f6f29acdd166226e91a4ae1a994c75
Eric Crosson <eric.s.crosson@utexas.edu>
2013-07-21 01:20:16 -0500

    Changed driver and interface for libre wireless card

--- nemo/wifi-start.sh
+++ nemo/wifi-start.sh
@@ -1,2 +1,10 @@
-sudo wpa_supplicant -B -c /etc/wpa_supplicant.conf -D wext -i wlp3s0
-sudo dhcpcd wlp3s0
+interface=wlp0s20u1
+sudo wpa_supplicant -B -c/etc/wpa_supplicant.conf -Dnl80211 -i${interface}
+
+# Wait for stability
+while [[ -z `ps -e | grep wpa_supplicant` ]]; do
+    sleep 1
+done
+sleep 1				# one more for good measure
+
+sudo dhcpcd ${interface}
