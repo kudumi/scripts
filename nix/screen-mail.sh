60171c50110db0df830bde29c222261f39f6f002
Eric Crosson <eric.s.crosson@utexas.edu>
2013-08-02 09:04:56 -0500

    Added xmail (screen for mail)

---
+++ nix/screen-mail.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+name=mailer
+screen -wipe			# clear out stale screens
+
+if [[ `screen -ls | grep "[0-9]*.$name"`  ]]; then
+    screen -x $name
+else
+    screen -S $name -c $HOME/Dropbox/config/screen/.screenrc.mail
+fi
