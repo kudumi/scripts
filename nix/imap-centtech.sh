96024fea2c64afee9917f25c1c64233d93920064
Eric Crosson <eric.s.crosson@utexas.edu>
2013-08-03 14:52:03 -0500

    Using the nightman for security now

--- nix/imap-centtech.sh
+++ nix/imap-centtech.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+auth="$HOME/.imapfilter/centtech.gpg"
+
+function ctrl-c() { nightman return ${auth}; }
+trap ctrl-c SIGINT		# Trap exits and interrupts to
+trap ctrl-c EXIT		# avoid leaving behind secrets
+
+nightman checkout ${auth}
+
 imapfilter -c $HOME/Dropbox/config/.imapfilter/centtech.rules.lua
