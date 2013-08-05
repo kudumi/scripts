1077837862715c798a0a0b044d1e6ea2a02a89bf
Eric Crosson <eric.s.crosson@utexas.edu>
2013-08-05 13:34:48 -0500

    Now using the nightman script

--- nix/imap-merchant-archive.sh
+++ nix/imap-merchant-archive.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+
+auth="$HOME/.imapfilter/esc.gpg"
+
+function ctrl_c () { nightman return ${auth}; }
+trap ctrl_c SIGINT		# Trap exits and interrupts to
+trap ctrl_c EXIT		# avoid leaving behind secrets
+
+nightman checkout ${auth}
+
 imapfilter -c $HOME/Dropbox/config/.imapfilter/archive.merchant.lua
