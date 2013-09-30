cee585f0a2dd180b9337012b4fc594033db86f38
Eric Crosson <eric.s.crosson@utexas.edu>
2013-09-30 17:38:33 -0500

    Minor core updates

--- nix/lock.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This script locks the screen via xscreensaver daemon
-[[ -z `xscreensaver-command -time 2>/dev/null` ]] && xscreensaver -no-splash &
-xscreensaver-command --activate
